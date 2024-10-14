f1a4d52add806d88a996832471037d435bdce23a
findingmulo <findingmulo@yahoo.com>
2024-10-13 21:39:51 -0400

    terraform code

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "nze-terraform"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "locktable"
+  }
+}
---
+++ ec2.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "server1" {
+  ami           = "ami-0195204d5dce06d99"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public1.id
+  security_groups = [aws_security_group.sg-demo.id]
+  /* or you can use 
+  vpc_security_group_ids = [aws_security_group.sg-demo.id]*
+  insteade of security_groups */
+  key_name               = aws_key_pair.aws_key.key_name
+  user_data              = file("server.sh")
+  tags = {
+    Name = "utc-app-inst"
+  }
+}
